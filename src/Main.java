f88a6afd33dc3ee2c7e8e52a337cefa5a6e49970
Tenphun0503_PC <tenzinphuntsok0503@gmail.com>
2023-02-15 02:39:06 -0500

    update content

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String myStr = "Hello World!";
+        myStr.length();
     }
 }
